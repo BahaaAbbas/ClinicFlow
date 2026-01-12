05efd41b4d0ee282b5b07f8e580625f013ada274
Bahaa Abbas <bahaaisl566@gmail.com>
2026-01-12 08:41:58 +0200

    Implemented Frontend Auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,27 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import "./App.css";
+import { AuthProvider } from "./context/AuthContext";
+import PatientDashboard from "./pages/PatientDashboard";
+import DoctorDashboard from "./pages/DoctorDashboard";
+import FinanceDashboard from "./pages/FinanceDashboard";
+
 function App() {
   return (
-    <>
-      <p>Bahaa Abbas </p>
-    </>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/patient" element={<PatientDashboard />} />
+          <Route path="/doctor" element={<DoctorDashboard />} />
+          <Route path="/finance" element={<FinanceDashboard />} />
+
+          <Route path="*" element={<Navigate to={"/"} />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,45 @@
+import { createContext, useState, useEffect } from "react";
+import { authAPI } from "../services/apiConfig";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
+  const checkAuth = async () => {
+    try {
+      const res = await authAPI.checkAuth();
+      if (res.data.authenticated) {
+        setUser(res.data.user);
+      }
+    } catch (error) {
+      console.error("Auth check failed:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const login = (userData) => {
+    setUser(userData);
+  };
+
+  const logout = async () => {
+    try {
+      await authAPI.logout();
+      setUser(null);
+    } catch (error) {
+      console.error("Logout failed:", error);
+    }
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
