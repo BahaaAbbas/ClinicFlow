c7f8efc674a7599907049a88015a93f795a38828
Bahaa Abbas <bahaaisl566@gmail.com>
2026-01-13 08:04:54 +0200

    deploy issue fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,6 +24,7 @@ function App() {
               </PrivateRoute>
             }
           />
+
           <Route
             path="/doctor"
             element={
@@ -32,6 +33,7 @@ function App() {
               </PrivateRoute>
             }
           />
+
           <Route
             path="/finance"
             element={
