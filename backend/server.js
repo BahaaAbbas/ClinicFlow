53b12854c2b733268ee5a6bbba33d37f698c30a8
Bahaa Abbas <bahaaisl566@gmail.com>
2026-01-13 07:49:04 +0200

    fix cors problem

--- backend/server.js
+++ backend/server.js
@@ -20,11 +20,10 @@ app.use(
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
+    exposedHeaders: ["Set-Cookie"],
   })
 );
-
-app.options("*", cors());
 
 app.use(express.json());
 app.use(cookieParser());
